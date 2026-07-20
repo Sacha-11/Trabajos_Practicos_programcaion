22317d386f327d9d3bfbdfcd02cef63d604569a6
Sacha <guardiandelmonte1@gmail.com>
2026-07-20 11:03:25 -0300

    Update carrera.py

--- POO/carrera.py
+++ POO/carrera.py
@@ -1,4 +1,4 @@
-from Personaje import personaje
+from Personaje import Personaje
 
 menu = '''
 1- Crear personaje
@@ -10,18 +10,21 @@ while  True:
     opcion = int(input("Ingrese una opcion"))
 
     if opcion == 1:
-        nombre=input("ingrese el nombre")
-        altura=float(input("Ingrese la altura del personaje"))
-        velocidad=int(input(""))
-        resistencia=int(input("Ingrese el valor de la resistencia"))
-        fuerza=int(input("Ingrese la fuerza del personaje"))
+        nombre=input("ingrese el nombre: ")
+        altura=float(input("Ingrese la altura del personaje: "))
+        velocidad=int(input("Ingrese la velocidad: "))
+        resistencia=int(input("Ingrese el valor de la resistencia: "))
+        fuerza=int(input("Ingrese la fuerza del personaje: "))
 
         personaje1=Personaje(nombre,altura,velocidad,resistencia,fuerza)
         print(personaje1)
 
     elif opcion == 2:
-        tiempo=Personaje.correr()
-        print("El personaje corrio", tiempo)
+        tiempo=personaje1.correr()
+        print(nombre, "corrio", tiempo, "en segundos")
 
+    elif tiempo < 200:
+        vivo = False
+        print (nombre, "a muerto")
     elif opcion == 3:
         break
